7f91aba55cbad37bfe499220b68817194b8711eb
Adrien Gaston <adriencgaston@outlook.com>
2023-03-31 18:23:01 -0400

    why is this a commit i literally changed nothing

--- main.lua
+++ main.lua
@@ -13,8 +13,7 @@
 
     You should have received a copy of the GNU Affero General Public License
     along with this program. If not, see https://www.gnu.org/licenses/agpl-3.0.en.html.
-]]
-   --
+]]--
 
 print("Hello, World!")
 
