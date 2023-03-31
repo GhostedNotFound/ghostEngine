dfdba4738144720ea908f6fa8e24e0ee31f3badd
Adrien Gaston <adriencgaston@outlook.com>
2023-03-31 18:21:25 -0400

    0.2.7 changes Changed documentation main.lua formatting

--- main.lua
+++ main.lua
@@ -13,7 +13,8 @@
 
     You should have received a copy of the GNU Affero General Public License
     along with this program. If not, see https://www.gnu.org/licenses/agpl-3.0.en.html.
-]] --
+]]
+   --
 
 print("Hello, World!")
 
@@ -22,17 +23,17 @@ love.window.setTitle("Ghost Engine: Initalizing")
 -- Define local variables & functions:
 
 GhostEngine = {
-    drawCustomTypes = function (k,v)
-        
+    drawCustomTypes = function(k, v)
+
     end,
-    frame = function ()
-        
+    frame = function()
+
     end
 }
 
 local GameLoaded = false
 local currentInternalError = nil
-local validTypes = {"text", "image", "rect", "circle"}
+local validTypes = { "text", "image", "rect", "circle" }
 local internalEngineErrors = {
     NoGameDetected =
     "Welcome to Ghost Engine!\nNo game.lua file detected.\nIs it in the right folder?\nRead the DOCUMENTATION file for help.\n\n[ [-- (E404) --] ]",
@@ -40,8 +41,8 @@ local internalEngineErrors = {
     "game.lua file detected, but couldn't be assessed.\nRead the DOCUMENTATION file for help.\n\n[ [-- (E403) --] ]",
     Test = "Success.\n\n[ [-- (E000) --] ]",
     IncompatiableSystem = "Your system (" ..
-    love.system.getOS() ..
-    ") is incompatible with Ghost Engine.\nWe apologize for the inconvenience.\n\n[ [-- (E100) --] ]"
+        love.system.getOS() ..
+        ") is incompatible with Ghost Engine.\nWe apologize for the inconvenience.\n\n[ [-- (E100) --] ]"
 }
 local function _errorScreen(c)
     if currentInternalError then
@@ -50,7 +51,7 @@ local function _errorScreen(c)
     love.window.setTitle("Ghost Engine: InternalEngineError" .. c)
     currentInternalError = c
     love.keyboard.setKeyRepeat(true)
-    print("SoftHalt:",c)
+    print("SoftHalt:", c)
     CreateUI("latestError", "text", { t = internalEngineErrors[c] .. "\n(Press ESC to quit, press CTRL+C to copy)" })
     love.graphics.setBackgroundColor(0.2, 0, 0, 1)
 end
@@ -64,8 +65,8 @@ local function _checkCompatSystem()
     if love.system.getOS() == "Windows" then
         print("[!] Running on Windows, may have problems!")
         love.window.showMessageBox("Incompatiable System Warning",
-        "You are currently running the open-source version of Ghost Engine on a Windows system.\nThe engine cannot check for a valid game.lua file.\nYou have been warned.",
-        "warning", false)
+            "You are currently running the open-source version of Ghost Engine on a Windows system.\nThe engine cannot check for a valid game.lua file.\nYou have been warned.",
+            "warning", false)
         return true
     end
     return true
@@ -80,11 +81,11 @@ local function _getGameDat()
             love.window.setTitle("Ghost Engine: The Newest Best Game?")
         end
     end
-    
+
     if GameDat.removeWatermark and GameDat.title then
         love.window.setTitle(GameDat.title)
     end
-    
+
     if GameDat.icon then
         print("Attempting to load Custom Icon...")
         love.window.setIcon(GameDat.icon)
@@ -93,8 +94,13 @@ local function _getGameDat()
 end
 
 local function _enforceHaltIf(condition, message)
-    if not condition then love.window.setTitle("Ghost Engine: EnforcedHaltViaAssertion") print("Halt Enbound!") end
-    assert(condition, message.."\n\nCheck your code, traceback is unhelpful here." or "Halt was enforced unexpectingly.\n\nCheck with the current maintainer of Ghost Engine.")
+    if not condition then
+        love.window.setTitle("Ghost Engine: EnforcedHaltViaAssertion")
+        print("Halt Enbound!")
+    end
+    assert(condition,
+    message .. "\n\nCheck your code, traceback is unhelpful here." or
+    "Halt was enforced unexpectingly.\n\nCheck with the current maintainer of Ghost Engine.")
 end
 
 local function _layersExist()
@@ -117,8 +123,14 @@ local function _doPredeterminedTypes(k, v)
     if v.type == "text" then
         love.graphics.print(v.data.t, v.data.x or 0, v.data.y or 0)
     elseif v.type == "image" then
-        _enforceHaltIf(file_exists(v.data.i),"Originated from CreateUI(" ..
-        k .. ", " .. v.type .. ", {i=\""..v.data.i.."\"}): Image (" .. v.data.i .. ") is non-existant.\nCurrent version of Ghost Engine has flaws, so halt was enforced.")
+        _enforceHaltIf(file_exists(v.data.i), "Originated from CreateUI(" ..
+            k ..
+            ", " ..
+            v.type ..
+            ", {i=\"" ..
+            v.data.i ..
+            "\"}): Image (" ..
+            v.data.i .. ") is non-existant.\nCurrent version of Ghost Engine has flaws, so halt was enforced.")
         love.graphics.draw(love.graphics.newImage(v.data.i), v.data.x or 0, v.data.y or 0)
     elseif v.type == "rect" then
         love.graphics.rectangle(v.data.mode or "line", v.data.x or 0, v.data.y or 0, v.data.w,
@@ -156,7 +168,7 @@ local function _drawOtherLayers()
         else
             for key, value in pairs(Layers[key]) do
                 _doPredeterminedTypes(key, value)
-                GhostEngine.drawCustomTypes(key, value) 
+                GhostEngine.drawCustomTypes(key, value)
             end
         end
     end
@@ -164,7 +176,7 @@ end
 
 -- Define global variables & functions:
 
-Layers = {BackgroundLayer = {},ForegroundLayer = {},UILayer = {}}
+Layers = { BackgroundLayer = {}, ForegroundLayer = {}, UILayer = {} }
 TICKS = 0
 _SCREENWIDTH = love.graphics.getWidth()
 _SCREENHEIGHT = love.graphics.getHeight()
@@ -204,7 +216,7 @@ function file_exists(name)
     if type(name) ~= "string" then return false end
     local f, err = io.open(name, "r")
     if err then return false end
----@diagnostic disable-next-line: need-check-nil
+    ---@diagnostic disable-next-line: need-check-nil
     f:close()
     return true
 end
@@ -219,31 +231,33 @@ function CreateObject(layer, id, typeOf, data)
     end
     if type(id) == "string" and type(layer) == "string" and type(data) == "table" and validType and Layers[layer] and not Layers[layer][id] then
         data["id"] = id
-        data["destroy"] = function (self)
-            print("Destroyed ObjectId "..self.id)
+        data["destroy"] = function(self)
+            print("Destroyed ObjectId " .. self.id)
             Layers[layer][self.id] = nil
         end
         Layers[layer][id] = { type = typeOf, data = data }
-        print("Created new Object in layer",layer,"with id",id,"as type",typeOf)
+        print("Created new Object in layer", layer, "with id", id, "as type", typeOf)
         return Layers[layer][id].data
     elseif not validType then
         CreateUI("latestError", "text",
             {
                 x = 0,
                 y = 0,
-                t = "CreateObject(...): \"" .. typeOf .. "\" is not a valid type. Error is not fatal, so halt wasn't enforced.",
+                t = "CreateObject(...): \"" ..
+                typeOf .. "\" is not a valid type. Error is not fatal, so halt wasn't enforced.",
                 clr = { 1, 0, 0 }
             })
         print("CreateObject(...): \"" .. typeOf .. "\" is not a valid type. Error is not fatal, so halt wasn't enforced.")
         return Layers.UILayer.latestError.data
     elseif (not Layers[layer]) or (Layers[layer][id]) then
         CreateUI("latestError", "text",
-        {
-            x = 0,
-            y = 0,
-            t = "CreateObject(...): Layer \"" .. layer .. "\" has conflict. Error is not fatal, so halt wasn't enforced.",
-            clr = { 1, 0, 0 }
-        })
+            {
+                x = 0,
+                y = 0,
+                t = "CreateObject(...): Layer \"" ..
+                layer .. "\" has conflict. Error is not fatal, so halt wasn't enforced.",
+                clr = { 1, 0, 0 }
+            })
         print("CreateObject(...): Layer \"" .. layer .. "\" has conflict. Error is not fatal, so halt wasn't enforced.")
         return Layers.UILayer.latestError.data
     else
@@ -265,8 +279,8 @@ function CreateUI(id, typeOf, data)
     if type(id) == "string" and validType and type(data) == "table" then
         print("Created UI object with id", id)
         data["id"] = id
-        data["destroy"] = function (self)
-            print("Destroyed ObjectId "..self.id)
+        data["destroy"] = function(self)
+            print("Destroyed ObjectId " .. self.id)
             Layers.UILayer[self.id] = nil
         end
         Layers.UILayer[id] = { type = typeOf, data = data }
@@ -276,10 +290,15 @@ function CreateUI(id, typeOf, data)
             {
                 x = 0,
                 y = 0,
-                t = "CreateUI(" .. id .. ", " .. typeOf .. "): \"" .. typeOf .. "\" is not a valid type. Error is not fatal, so halt wasn't enforced.",
+                t = "CreateUI(" ..
+                id ..
+                ", " ..
+                typeOf .. "): \"" .. typeOf .. "\" is not a valid type. Error is not fatal, so halt wasn't enforced.",
                 clr = { 1, 0, 0 }
             })
-        print("CreateUI(" .. id .. ", " .. typeOf .. "): \"" .. typeOf .. "\" is not a valid type. Error is not fatal, so halt wasn't enforced.")
+        print("CreateUI(" ..
+        id ..
+        ", " .. typeOf .. "): \"" .. typeOf .. "\" is not a valid type. Error is not fatal, so halt wasn't enforced.")
         return Layers.UILayer.latestError.data
     else
         CreateUI("latestError", "text",
@@ -290,8 +309,8 @@ function CreateUI(id, typeOf, data)
 end
 
 function DestroyUI(id)
-    print("Destroyed UI object with id", id)
-    if id and Layers.UILayer[id] then
+    if type(id) == "string" and Layers.UILayer[id] then
+        print("Destroyed UI object with id", id)
         Layers.UILayer[id] = nil
     else
         CreateUI("latestError", "text", {
