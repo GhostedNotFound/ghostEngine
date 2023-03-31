4542f9ddba63dfc03b0f349bc2a34210c318a0d6
Adrien Gaston <adriencgaston@outlook.com>
2023-03-31 18:46:01 -0400

    The GitHub Update

--- main.lua
+++ main.lua
@@ -2,17 +2,16 @@
     Copyright (C)
 
     This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at our option) any later version.
+    it under the terms of the MIT license as published by the 
+    Open Source Initiative.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU Affero General Public License for more details.
+    MIT license for more details.
 
-    You should have received a copy of the GNU Affero General Public License
-    along with this program. If not, see https://www.gnu.org/licenses/agpl-3.0.en.html.
+    You should have received a copy of the MIT license along with this program. 
+    If not, see https://opensource.org/license/mit/.
 ]]--
 
 print("Hello, World!")
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Ghost Engine (0.2.7)
+
+Ghost Engine is a somewhat lightweight engine that uses the LOVE framework, commonly used by Uncoded Studios to make game development easier.
+
+## Installation
+
+Put main.lua in a directory with a game.lua file, then run the directory via LOVE (or use your terminal to run LOVE in the directory.)
+
+Example:
+
+```bash
+$ love Documents/MyGame/
+```
+
+## Usage
+
+See wiki.
+
+## Support
+
+Contact me on discord at Ghost Only#7451.
+
+## License
+
+This project uses the [MIT](https://choosealicense.com/licenses/mit/) license. Check the files for a license file if you downloaded it.
+
+## Project Status
+
+In development. I recommend you wait for the 1.0.0 release before using this engine, but that's just me.
